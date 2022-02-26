fa4d2e18e64f083630a48e1fd3658e86e90562a8
liawesomesaucer <vincent.oddrepublic@gmail.com>
2022-02-25 21:26:06 -0800

    no longer waits for document to finish loading to be gray, also absolutely positioned at the highest level

--- README.md
+++ README.md
@@ -10,11 +10,6 @@ I built this for myself to use so currently it only works on facebook, reddit, a
 
 The intention of this application to provide (1) minor annoyance/friction and (2) constant reminding that you are using up precious time by being annoyingly bland. If you're actually trying to fully block websites, try editing your `/etc/hosts` (if on mac) or try something that can't be as easily removed as a chrome extension.
 
-## Future planned work (for v1)
-
-* Make this individually configurable for websites
-* More ways to block
-
 ## Why don't you just set the entire screen to black, Vincent?
 
 I want my syntax highlighting. My phone runs black and white most of the time. You can trust modern accessibility standards and efforts to put an overwhelming majority of content in a good place :)
--- tuxedo.js
+++ tuxedo.js
@@ -1,3 +1,20 @@
-// So I want to insert a script onto the page
-const elem = '<div class="tuxedo"></div>'
-document.body.insertAdjacentHTML('afterbegin', elem);
+/**
+ * We insert twice, once to begin with and once at the end. The first insert happens
+ * when body is ready, which allows us to start the load with the gray filter. The
+ * second load prevents anyone else from creating an element that could exist above
+ * the filter (z-index-wise)
+ */
+var observer = new MutationObserver(function() {
+  if (document.body) {
+    document.body.insertAdjacentHTML('afterbegin', '<div id="tuxedo-1" class="tuxedo"></div>');
+    observer.disconnect();
+  }
+});
+observer.observe(document.documentElement, {childList: true});
+
+document.addEventListener('DOMContentLoaded', () => {
+  // beforeend as lowest one down appears at the top
+  document.body.insertAdjacentHTML('beforeend', '<div id="tuxedo-2" class="tuxedo"></div>');
+  // Remove the original
+  document.getElementById('tuxedo-1').remove()
+});
